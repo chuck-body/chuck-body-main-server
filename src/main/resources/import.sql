64a544ad807361e239afbaf5540aa958100eccb3
yunsseong <aster20@hanyang.ac.kr>
2025-06-08 01:30:42 +0900

    feat: 기본 데이터 추가

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO doctor (id, name) VALUES(1, '김철수');
+INSERT INTO doctor (id, name) VALUES(2, '오현우');
+
+INSERT INTO patient (id, name, birth_date) VALUES (1, '김민준', '1980-05-15');
+INSERT INTO patient (id, name, birth_date) VALUES (2, '이서준', '1975-10-20');
+INSERT INTO patient (id, name, birth_date) VALUES (3, '박예준', '1990-03-25');
+INSERT INTO patient (id, name, birth_date) VALUES (4, '최도윤', '1985-12-10');
+INSERT INTO patient (id, name, birth_date) VALUES (5, '정시우', '1978-07-30');
