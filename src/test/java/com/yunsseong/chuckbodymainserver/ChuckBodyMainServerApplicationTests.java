ebfc616f18386079f9eb787b68a6381075c5a192
yunsseong <aster20@hanyang.ac.kr>
2025-06-07 21:40:13 +0900

    chore: 스프링 초기 설정

---
+++ src/test/java/com/yunsseong/chuckbodymainserver/ChuckBodyMainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsseong.chuckbodymainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckBodyMainServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
